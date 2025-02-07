d20d1cd71cc562a8a5f8131ae722c45ab3112d56
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-02-07 16:02:01 +0100

    complete the auth in the web3

---
+++ meta/metamask-connection-with-wagmi/pages/_app.tsx
@@ -0,0 +1,14 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { WagmiWrapper } from "@/wagmi";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiWrapper>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </WagmiWrapper>
+  );
+}
