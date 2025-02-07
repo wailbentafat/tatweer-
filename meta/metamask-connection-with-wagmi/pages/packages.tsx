5eb2e0530df0d0f8879e260322772ef25f108d37
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-02-07 17:39:03 +0100

    fix the layout

---
+++ meta/metamask-connection-with-wagmi/pages/packages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '@/components/layout';
+
+const PackagesPage = () => {
+  return (
+    <Layout>
+      <div>Packages Content</div>
+    </Layout>
+  );
+};
+
+export default PackagesPage;
---
+++ meta/metamask-connection-with-wagmi/pages/risk_alerts.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '@/components/layout';
+
+const RiskAlerts = () => {
+  return (
+    <Layout>
+      <div>Risk Alerts Content</div>
+    </Layout>
+  );
+};
+
+export default RiskAlerts;
